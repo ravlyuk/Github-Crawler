5dd350c5ee47c39e56e0ed502f5fbe7ff6bcdc1c
Yevhenii Ravliuk <41680988+ravlyuk@users.noreply.github.com>
2025-12-07 14:41:13 +0200

    Removes unused settings dependencies and simplifies config

--- src/config.py
+++ src/config.py
@@ -1,19 +1,17 @@
-from pydantic_settings import BaseSettings
-
-
 class Connection:
     """Connection settings."""
 
-    BASE_URL: str = "https://github.com"
-    SEARCH_URL: str = "https://github.com/search"
-    CHECK_URL: str = "https://icanhazip.com/"
-    TIMEOUT_SECOND: int = 5
-    TIMEOUT_FILTER_SECOND: int = 5
-    MAX_CONNECTIONS: int = 20
+    BASE_URL = "https://github.com"
+    SEARCH_URL = "https://github.com/search"
+    CHECK_URL = "https://icanhazip.com/"
+    TIMEOUT_SECOND = 5
+    TIMEOUT_FILTER_SECOND = 5
+    MAX_CONNECTIONS = 20
 
 
 class Path:
     """XPath expressions for parsing HTML."""
+
     ITEM_LINK_XPATH = '//div[contains(@class, "search-title")]/a/@href'
     CSS_VALUE_XPATH = '//a[contains(@href, "search?l=css")]/span[2]/text()'
     HTML_VALUE_XPATH = '//a[contains(@href, "search?l=html")]/span[2]/text()'
